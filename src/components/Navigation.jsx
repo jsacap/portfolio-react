d5808450f1612b5d7dd28841e1f706fae01f7e59
jsacap <sanchoalegre@gmail.com>
2024-01-09 10:59:32 +1100

    React icons in navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,6 +3,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { AiFillGithub } from 'react-icons/ai';
 import { Link, NavLink } from 'react-router-dom';
+import { FaCode, FaChartLine } from 'react-icons/fa';
+import { HStack } from '@chakra-ui/react';
 
 export function Navigation() {
   const isLoggedIn = () => {
@@ -19,10 +21,15 @@ export function Navigation() {
       <Navbar className='navbar-transparent' variant="dark">
         <Navbar.Brand href="/" className='brand-padding'>Sancho Jr. Alegre</Navbar.Brand>
         <Nav className="me-auto">
+          <HStack>
+          <FaCode />
           <Nav.Link href="/">Projects</Nav.Link>
+          <FaChartLine />
           <Nav.Link href="/marketblog">Market Insights</Nav.Link>
+          </HStack>
         </Nav>
         <Nav>
+          <HStack>
           <Link to='https://github.com/jsacap' target='__blank' rel='noopener noreferror'>
             <AiFillGithub padding={5} color='white' />
           </Link>
@@ -30,7 +37,7 @@ export function Navigation() {
           {isLoggedIn() && <Nav.Link href='/tags'>Tags</Nav.Link>}
           {isLoggedIn() && <Nav.Link href='/logout'>Logout</Nav.Link>}
           {/* {isLoggedIn() ? <Nav.Link href='/logout'>Logout</Nav.Link> : <Nav.Link href="/login" className='brand-padding'>Login</Nav.Link>} */}
-
+          </HStack>
         </Nav>
       </Navbar>
     </div>
