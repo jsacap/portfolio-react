187c34da87ddbd3817f7bf755e7676e183192d9b
jsacap <sanchoalegre@gmail.com>
2024-01-09 16:15:04 +1100

    navbar mobile responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,45 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Box, HStack, IconButton, useDisclosure, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton,
+  Link, VStack
+} from '@chakra-ui/react';
+import { FaHamburger } from 'react-icons/fa';
 import { AiFillGithub } from 'react-icons/ai';
-import { Link, NavLink } from 'react-router-dom';
 import { FaCode, FaChartLine } from 'react-icons/fa';
-import { HStack } from '@chakra-ui/react';
 
 export function Navigation() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   const isLoggedIn = () => {
     const accessToken = localStorage.getItem('accessToken');
     return !!accessToken; 
   };
-  
-
-  
 
   return (
-    <div>
-      
-      <Navbar className='navbar-transparent' variant="dark">
-        <Navbar.Brand href="/" className='brand-padding'>Sancho Jr. Alegre</Navbar.Brand>
-        <Nav className="me-auto">
-          <HStack>
-          <FaCode />
-          <Nav.Link href="/">Projects</Nav.Link>
-          <FaChartLine />
-          <Nav.Link href="/marketblog">Market Insights</Nav.Link>
-          </HStack>
-        </Nav>
-        <Nav>
-          <HStack>
-          <Link to='https://github.com/jsacap' target='__blank' rel='noopener noreferror'>
-            <AiFillGithub padding={5} color='white' />
-          </Link>
-          {isLoggedIn() && <Nav.Link href='/post'>New Project Post</Nav.Link>}
-          {isLoggedIn() && <Nav.Link href='/tags'>Tags</Nav.Link>}
-          {isLoggedIn() && <Nav.Link href='/logout'>Logout</Nav.Link>}
-          {/* {isLoggedIn() ? <Nav.Link href='/logout'>Logout</Nav.Link> : <Nav.Link href="/login" className='brand-padding'>Login</Nav.Link>} */}
-          </HStack>
-        </Nav>
-      </Navbar>
-    </div>
+    <Box>
+      <HStack justifyContent="space-between" p={4}>
+        <Link as={RouterLink} to="/" fontWeight="bold">
+          Sancho Jr. Alegre
+        </Link>
+        <IconButton
+          ref={btnRef}
+          icon={<FaHamburger />}
+          onClick={onOpen}
+          display={{ base: "inline-flex", md: "none" }}
+        />
+      </HStack>
+
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody>
+            <VStack align="start">
+              <Link as={RouterLink} to="/" onClick={onClose}>Projects</Link>
+              <Link as={RouterLink} to="/marketblog" onClick={onClose}>Market Insights</Link>
+              {isLoggedIn() && <Link as={RouterLink} to="/post" onClick={onClose}>New Project Post</Link>}
+              {isLoggedIn() && <Link as={RouterLink} to="/tags" onClick={onClose}>Tags</Link>}
+              {isLoggedIn() && <Link as={RouterLink} to="/logout" onClick={onClose}>Logout</Link>}
+              {/* ... other links ... */}
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
   );
 }
