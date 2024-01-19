eaac755bf86cbde9d2f8b1a918ea04bc30bf2073
jsacap <sanchoalegre@gmail.com>
2024-01-19 11:52:02 +1100

    Skills complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,49 +1,16 @@
-import React, { useEffect } from 'react';
-import Typed from 'typed.js';
-import LatestProjects from './LatestProjects';
-import { Box } from '@chakra-ui/react';
-import { SiDjango, SiReact, SiJavascript, SiHtml5, SiCss3, SiSkillshare } from 'react-icons/si'; // Import other icons as needed
-import Slider from 'react-slick';
+import React from 'react';
 import Skills from './skills';
+import Header from './Header'
 
 
 const Home = () => {
 
-    const bgImage = "url('/home/hero-bg.jpg')";
-
-    useEffect(() => {
-        const typed = new Typed('.typed', {
-            strings: ["FX Trader", "Investor", "Full-Stack Developer", "Data Analyst", "Freelancer"],
-            loop: true,
-            typeSpeed: 50,
-            backSpeed: 40,
-            backDelay: 2000
-        });
-
-        return () => {
-            typed.destroy();
-        };
-    }, []);
+    
 
     return (
         <>
-        <div id="hero" className="home__hero route bg-image" style={{ backgroundImage: bgImage }}>
-            <div className='overlay-itro'></div>
-            <div className='home__hero-content display-table'>
-                <div className='table-cell'>
-                    <div className='container'>
-                        <h1 className='home__hero-title mb-b'>Sancho Jr Alegre</h1>
-                        <p className='home__hero-subtitle'>
-                            <span className='typed'></span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-        <Skills />
+            <Header />
+            <Skills />
         
         </>
     )
--- src/pages/home/Skills.jsx
+++ src/pages/home/Skills.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css';
-import { SiDjango, SiReact, SiJavascript, SiHtml5, SiCss3 } from 'react-icons/si';
+import { SiDjango, SiReact, SiJavascript, SiHtml5, SiCss3, SiStreamlit, SiNumpy, SiPlotly, SiPandas } from 'react-icons/si';
+import { DiDjango } from "react-icons/di";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+
 
 const Skills = () => {
     const iconSize = '30px';
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 10000,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true, 
-        autoplaySpeed: 5000,
+        autoplaySpeed: 500,
         pauseOnHover: true
       };
       
@@ -25,7 +29,10 @@ const Skills = () => {
         <div className='container' data-aos='fade-up'>
             <div className='section-title'>
             <h2>Full-Stack Development</h2>
-            <p>Developer skills for both frontend and backend technologies as well as database and APIs.</p>
+            <p>
+                Skills spans across the full spectrum of web development, encompassing front-end 
+                and back-end technologies, database management, and API integration.
+            </p>
             </div>
 
             <Slider {...settings}>
@@ -59,6 +66,43 @@ const Skills = () => {
                     <p>CSS3 - Style sheet language used for describing the presentation of a document written in a markup language.</p>
                     </div>
                 </div>
+                <div>
+                    <div className='skills-item'>
+                        <SiPandas size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>Pandas - A powerful data analysis and manipulation library for Python.</p>
+                    </div>
+                    </div>
+                    <div>
+                    <div className='skills-item'>
+                        <SiStreamlit size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>Streamlit - An open-source app framework for Machine Learning and Data Science teams.</p>
+                    </div>
+                    </div>
+                    <div>
+                    <div className='skills-item'>
+                        <SiNumpy size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>NumPy - A fundamental package for scientific computing with Python.</p>
+                    </div>
+                    </div>
+                    <div>
+                    <div className='skills-item'>
+                        <SiPlotly size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>Plotly - A graphing library that makes interactive, publication-quality graphs online.</p>
+                    </div>
+                    </div>
+                    <div>
+                    <div className='skills-item'>
+                        <DiDjango size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>Django REST API - A powerful and flexible toolkit for building Web APIs with Django.</p>
+                    </div>
+                    </div>
+                    <div>
+                    <div className='skills-item'>
+                        <BiLogoPostgresql size={iconSize} /> {/* Replace with actual icon if available */}
+                        <p>PostgreSQL - A powerful, open source object-relational database system.</p>
+                    </div>
+                    </div>
+
                 </Slider>
         </div>
         </section>
