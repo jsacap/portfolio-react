1f969f77d88beb8c8223e99a176bc6d32f63ba83
jsacap <sanchoalegre@gmail.com>
2024-03-02 08:57:52 +1100

    lazyload BlogCard img

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Heading, Text, VStack, Image, Badge, HStack, Skeleton } from '@chakra-ui/react';
 import parse from 'html-react-parser';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 const BlogCard = ({ id, coverPhoto, title, content, tags, created }) => {
-  const [imageLoaded, setImageLoaded] = useState(false); // Image loading state
+  const [imageLoaded, setImageLoaded] = useState(false); 
+  const [parsedContent, setParsedContent] = useState('');
   const cardVariants = {
     initial: { opacity: 0, y: 10 },
     enter: { opacity: 1, y: 0 },
     hover: { scale: 1.05 }
-  }
+  };
   const navigate = useNavigate();
 
   const handlePostClick = () => {
-    navigate(`/blog/article/${id}`)
-  }
-  
+    navigate(`/blog/article/${id}`);
+  };
+
+  useEffect(() => {
+    const contentPreview = content.slice(0, 300);
+    setParsedContent(contentPreview);
+  }, [content]);
+
   return (
     <Box
       data-aos='flip-up' data-aos-duration='2000' data-aos-once='true'     
@@ -33,8 +39,8 @@ const BlogCard = ({ id, coverPhoto, title, content, tags, created }) => {
         variants={cardVariants}
         initial="initial"
         animate="enter"
-        whileHover="hover">
-        
+        whileHover="hover"
+      >
         {!imageLoaded && (
           <Skeleton height="300px" width="100%" fadeDuration={1} />
         )}
@@ -44,13 +50,13 @@ const BlogCard = ({ id, coverPhoto, title, content, tags, created }) => {
           width="100%"
           height="300px"
           objectFit="cover"
-          display={imageLoaded ? "block" : "none"} // Only display the image when it's loaded
-          onLoad={() => setImageLoaded(true)} // Update loading state when image is loaded
+          display={imageLoaded ? "block" : "none"} 
+          onLoad={() => setImageLoaded(true)} 
+          loading="lazy" 
         />
-
         <VStack p="4" align="start">
           <Heading size="md">{title}</Heading>
-          {parse(content)}
+          <Text>{parsedContent}...</Text> {/* Display the sliced content as the preview */}
           <HStack spacing={2} flexWrap='wrap'>
             {tags.map(tagName => (
               <Badge key={tagName} borderRadius="full" px="2" colorScheme="teal">
