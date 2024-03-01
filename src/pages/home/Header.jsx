92c7adbfbb3c703ee332954c25f57ec85349a7d6
jsacap <sanchoalegre@gmail.com>
2024-03-02 07:22:33 +1100

    blurhash on header

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Typed from 'typed.js';
+import { Blurhash } from 'react-blurhash';
 
 const Home = () => {
-    const bgImage = "url('/home/bgimage.png')";
+    // Adjust the image URL for direct use in the Image's src attribute
+    const bgImageUrl = '/home/bgimage.png';
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.onload = () => {
+            setImageLoaded(true);
+        };
+        img.src = bgImageUrl;
+    }, [bgImageUrl]);
 
     useEffect(() => {
         const typed = new Typed('.typed', {
@@ -19,20 +30,26 @@ const Home = () => {
     }, []);
 
     return (
-        <div id="hero" className="home__hero route bg-image" style={{ backgroundImage: bgImage }}>
-            <div className='overlay-itro'></div>
-            <div className='home__hero-content display-table'>
-                <div className='table-cell' data-aos='zoom-in-down' data-aos-once='true' data-aos-duration='5000' data-aos-easing='ease-in'>
-                    <div className='container'>
-                        <h1 className='home__hero-title mb-b'>Sancho Jr Alegre</h1>
-                        <p className='home__hero-subtitle'>
-                            <span className='typed'></span>
-                        </p>
+        <>
+            {!imageLoaded && (
+                <Blurhash hash='LEBDD^~q%N%Mo#oz%1xtIAM{M|IU' width='100%' height={600} resolutionX={32} resolutionY={32} punch={1} />
+            )}
+            
+            <div id="hero" className="home__hero route bg-image" style={imageLoaded ? { backgroundImage: `url(${bgImageUrl})` } : {}}>
+                <div className='overlay-itro'></div>
+                <div className='home__hero-content display-table'>
+                    <div className='table-cell' data-aos='zoom-in-down' data-aos-once='true' data-aos-duration='5000' data-aos-easing='ease-in'>
+                        <div className='container'>
+                            <h1 className='home__hero-title mb-b'>Sancho Jr Alegre</h1>
+                            <p className='home__hero-subtitle'>
+                                <span className='typed'></span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </>
+    );
 }
 
 export default Home;
